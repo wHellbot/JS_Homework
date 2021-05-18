bce53a9e5b5ce07b350c050c7c8b34ce1da9510d
Kirill Klimashevich <whellbotw@gmail.com>
2021-05-19 00:59:22 +0300

    Homework 15

---
+++ Homework 15/scripts.js
@@ -0,0 +1,94 @@
+var x = document.getElementById('x');
+var y = document.getElementById('y');
+var buttonCreate = document.getElementById('button_create');
+buttonCreate.disabled = true;
+var body = document.getElementById('body');
+var newTable = document.createElement('table');
+
+
+
+function inputX() {
+    buttonCreate.disabled = true;
+    disButton();
+}
+
+function inputY() {
+    buttonCreate.disabled = true;
+    disButton();
+}
+
+function disButton() {
+    if (x.value.length > 0 && y.value.length > 0 && x.value !== ' ' && y.value !== ' ') {
+        buttonCreate.disabled = false;
+    }
+}
+
+buttonCreate.onclick = function (event) {
+    var oldTable = body.lastChild;
+
+    if (oldTable) {
+        body.replaceChild(newTable, oldTable);
+    } else {
+        newTable.innerHTML = '<table id="tb"></table>';
+        body.appendChild(newTable);
+    }
+
+    if (10 >= y.value > 0 && +y.value > 0 && y.value % 1 === 0) {
+        for (var i = 1; i <= y.value; i++) {
+            var newTd = document.createElement('tr');
+            newTd.innerHTML = '<td></td>';
+            newTable.appendChild(newTd);
+        }
+    } else {
+        alert('Введите корректное значение в поле Y - целое число от 1 до 10.');
+        y.value = '';
+        buttonCreate.disabled = true;
+    }
+
+    if (10 >= x.value > 0 && +x.value > 0 && x.value % 1 === 0) {
+        var allTd = newTable.children;
+        for (var k = 0; k < x.value - 1; k++) {
+            for (var i = 0; i < y.value; i++) {
+                var newTr = document.createElement('td');
+                newTr.innerHTML = '<td></td>';
+                allTd[i].appendChild(newTr);
+            }
+        }
+    } else {
+        alert('Введите корректное значение в поле X - целое число от 1 до 10.');
+        x.value = '';
+        buttonCreate.disabled = true;
+    }
+    y.value = '';
+    x.value = '';
+    buttonCreate.disabled = true;
+
+    var fullTable = newTable.children;
+    for (var n = 0; n < Array.from(fullTable).length; n++) {
+        var fullTd = Array.from(fullTable)[n].children;
+        var links_as_arr = Array.from(fullTd);
+        if (n % 2 !== 0) {
+            for (var m = 0; m < links_as_arr.length; m++) {
+                if (m % 2 === 0) {
+                    links_as_arr[m].className = 'black';
+                }
+            }
+        } else {
+            for (var p = 0; p < links_as_arr.length; p++) {
+                if (p % 2 !== 0) {
+                    links_as_arr[p].className = 'black';
+                }
+            }
+        }
+    }
+}
+
+newTable.onclick = function (event) {
+    var target = event.target;
+    if (target.tagName === 'TD') {
+        var eachTd = document.getElementsByTagName('TD');
+        for (var i = 0; i < Array.from(eachTd).length; i++) {
+            Array.from(eachTd)[i].classList.toggle('black');
+        }
+    }
+}
